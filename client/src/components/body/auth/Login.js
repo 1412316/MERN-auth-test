5f0f0a5b824c4d216a0675ebf077cdf336333698
NhatMinh <nhatminhdang1996@gmail.com>
2020-11-19 16:55:11 +0700

    make it better

--- client/src/components/body/auth/Login.js
+++ client/src/components/body/auth/Login.js
@@ -78,11 +78,11 @@ function Login() {
         err: err.response.data.msg,
         success: ''
       })
+      console.log(err)
     }
   }
 
   const responseFacebook = async (response) => {
-    console.log(response)
     try {
       const {accessToken, userID} = response
       const res = await axios.post('/user/facebook_login', {accessToken, userID})
@@ -146,6 +146,7 @@ function Login() {
           clientId="696386700626-8roq3k476vvt9p5vc1j5iq45ema714dn.apps.googleusercontent.com"
           buttonText="Login with google"
           onSuccess={responseGoogle}
+          onFailure={responseGoogle}
           cookiePolicy={'single_host_origin'} 
         />
         <FacebookLogin
